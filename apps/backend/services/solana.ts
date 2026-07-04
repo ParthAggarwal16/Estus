209e4c0ff53ebfe195594c83beb51ccf53567fca
ParthAggarwal16 <parth22012006@gmail.com>
2026-07-04 18:05:46 +0530

    get token balanes

--- apps/backend/services/solana.ts
+++ apps/backend/services/solana.ts
@@ -23,3 +23,24 @@ export async function getNativeBalance(rpcUrl: string, address: string) {
     sol: balance / LAMPORTS_PER_SOL,
   }
 }
+
+export async function getTokenBalances(rpcUrl: string, publicKey: string) {
+
+  const connection = getConnection(rpcUrl)
+
+  const TOKEN_PROGRAM_ID = new PublicKey("TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA")
+  const tokenAccounts = await connection.getParsedTokenAccountsByOwner(new PublicKey(publicKey), { programId: TOKEN_PROGRAM_ID })
+
+  return tokenAccounts.value.map(({ account }) => {
+    const info = account.data.parsed.info;
+    const tokenAmount = info.tokenAmount;
+
+    return {
+      mintAddress: info.mint,
+      amount: tokenAmount.uiAmount,
+      decimals: tokenAmount.decimals,
+      rawAmount: tokenAmount.amount,
+    }
+  })
+
+}
