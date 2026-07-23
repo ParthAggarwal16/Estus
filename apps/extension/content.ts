1cc7dcd7da6def2f3ec42a4764b19576dafeab02
ParthAggarwal16 <parth22012006@gmail.com>
2026-07-23 23:50:52 +0530

    adding windows.eventListener to content.ts

--- apps/extension/content.ts
+++ apps/extension/content.ts
@@ -8,4 +8,14 @@ script.type = "text/javascript";
 
 script.onload = () => script.remove()
 
+window.addEventListener("message", (event) => {
+  if (event.source !== window) return
+
+  if (event.data?.target !== "estus-content") return
+
+  console.log("Content received:", event.data)
+
+  chrome.runtime.sendMessage(event.data)
+})
+
 console.log("[Estus] Provider injected")
